c13e87aa862c8fde4397c6fcb9fb2e258d038431
hejianglin <hejianglin@uino.com>
2021-06-23 09:30:37 +0800

    direct proxy

---
+++ proxy.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const request = require('request')
+
+const app = express()
+
+const port = process.env.PORT || 4000
+
+
+app.use('/', function (req, res) {
+	  const { url } = req;
+	  const urls = `https://apk.tools${url}`
+	  console.log(urls); 
+	  const html = request({url: urls, timeout: 2000},((err) => {
+		if(err) {
+		  res.status = 404;
+		  res.end("TIMES OUT PROXY ERROR")
+		  return;
+		}
+	  }));
+	  req.pipe(html).pipe(res)
+})
+
+app.use('/favicon.ico', function (req, res) {
+	res.sendFile('./assets/favicon.ico')
+})
+
+app.listen(port)
+
+console.log(`app listening on  4000 port`);
